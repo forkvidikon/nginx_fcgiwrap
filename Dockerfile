a40c61181c62694008b36f3893fe9df0dd52f4df
vidikon <admin@vidikon.info>
2023-04-22 00:30:04 +0300

    Добавлен генератор qr кода

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 # Указываем референсный образ
 FROM alpine:3.17
+
 # Меняем зеркало репозитория на яндекс. Шаг не обязательный, у меня есть проблемы с доступом к стандартному репозиторию
-RUN echo "https://mirror.yandex.ru/mirrors/alpine/v3.17/main" > /etc/apk/repositories
+#RUN echo "https://mirror.yandex.ru/mirrors/alpine/v3.17/main" > /etc/apk/repositories
+
 # Устанавливаем nginx и fcgiwrap
-RUN  apk add  --no-cache nginx fcgiwrap
+RUN  apk add  --no-cache nginx fcgiwrap libqrencode
 # Закидываем внутрь контейнера скрипт который делает всякие полезные штуки (читай коментарии в скрипте). Запекаем его в образе
 COPY sh/service.sh /service.sh
 # И задаем ему права на исполнение
--- data/index.sh
+++ data/index.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env ash
+
 echo "Content-type: text/html"
 echo ""
-now="$(date)"
-echo '<html><head><title>Hello World - CGI app</title></head>'
-echo '<body>'
- 
-echo '<h2>Hello World!</h2>'
-echo "Computer name : $HOSTNAME<br/>"
-echo "The current date and time : ${now}<br/>"
-echo '</body>'
-echo '</html>'
+
+#Заглушка - имя изображения, содержащего qr код
+imagename="qrcode.png"
+
+# Заглушка - страница, на которую переходит пользователь после сканирования кода
+autchPage="auth.html"
+
+# Генерируем qr код
+qrencode -d 320 -s 6 -l H -o "/var/www/html/img/$imagename" "http://$HTTP_HOST/cgi/index.sh"
+
+# отдаем пользователю html страницу, заменя переменные на реальные данные
+cat /var/www/html/qr.html | sed "s|QRCODE_SECTION|/img/$imagename|g" | sed "s|QR_PAGE_AUTH|$autchPage|g"
+
+
