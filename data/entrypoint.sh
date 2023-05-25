9fe165f5f7837d8d67fc911874916fea8f01978a
vidikon <admin@vidikon.info>
2023-05-25 14:30:47 +0300

    check qr code id

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -6,11 +6,33 @@ echo ""
 
 qrid=$(echo "$QUERY_STRING" | awk -F "=" '{ print $2 }')
 
+dateloadpage=$(date +%s)
+
+request="$(http --ignore-stdin "http://admin:admin@couchserver:5984/qrlive/$qrid")"
+
+if [[ "$(echo "$request" | jq -r .error)" == "not_found" ]]; then
+    response="Ошибка. Неверный ID - $qrid"
+
+        else
+
+        startlive="$(echo "$request" | jq -r .timestamp)"
+
+let "timefailed=dateloadpage-startlive "
+
+if [ $timefailed  -gt 240 ] ; then 
+
+    response="Ошибка. QR од с ID $qrid устарел, а ты морковка"
+
+    else
+        response="ID $qrid прошел проверку"
+        fi
+
+fi
 
 # отдаем пользователю html страницу, заменя переменные на реальные данные
-cat /var/www/html/entrypoint.html | sed "s|QRCODEID|$qrid|g" 
+cat /var/www/html/entrypoint.html | sed "s|QRCODEID|$response|g" 
 
 
-env
+#env
 
 
--- data/index.sh
+++ data/index.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env ash
+#!/bin/ash
+
 
 echo "Content-type: text/html"
 echo ""
+
 
 #Заглушка - имя изображения, содержащего qr код
 imagename="qrcode.png"
@@ -10,6 +12,8 @@ imagename="qrcode.png"
 autchPage="entrypoint.sh"
 
 
+        # Создать базу, в которой будем отслеживать qr-коды. Если уже существует, то создана не будет
+     #   http PUT "http://admin:admin@couchserver:5984/qrlive"
 
 
     # Провека на существование файла блокировки
@@ -32,10 +36,17 @@ autchPage="entrypoint.sh"
         # &>/dev/null используется для подавления любого вывода, чтобы он случайно не оказался на сайте
         nohup /var/sh/cgi/./lock.sh $dategenqr $block &>/dev/null &
         #Ссылка для qr-кода
-        link=$(echo "http://$HTTP_HOST/cgi/$autchPage?qrid=$RANDOM")
+        qrcodeid="$RANDOM"
+        link=$(echo "http://$HTTP_HOST/cgi/$autchPage?qrid=$qrcodeid")
 
         # Генерируем qr код
         qrencode -d 320 -s 6 -l H -o "/var/www/html/img/$imagename" "$link"
+
+
+
+       http --ignore-stdin -q PUT "http://admin:admin@couchserver:5984/qrlive/$qrcodeid"  timestamp="$dategenqr"
+      
+
         
     fi
 
