c1f2e188de247f59b665563274ac09b346e48383
Alexey <sotchenkoff@gmail.com>
2023-06-07 21:21:09 +0300

    Added long-live cookie setting

--- data/access.sh
+++ data/access.sh
@@ -2,11 +2,12 @@
 
 
 cookie_allow="$(echo $HTTP_COOKIE | awk -F "=" '{ print $2 }')"
+llc="$(echo $HTTP_COOKIE | awk -F "=" '{ print $1 }')"
+
+qr_allow_request="$(http --ignore-stdin "http://admin:admin@couchserver:5984/qrallow/$cookie_allow")"
 
 # Проверяем, есть ли у нас qrallow пользователя
-request="$(http --ignore-stdin "http://admin:admin@couchserver:5984/qrallow/$cookie_allow")"
-
-if [[ "$(echo "$request" | jq -r .error)" == "not_found" ]];
+if [[ "$(echo "$qr_allow_request" | jq -r .error)" == "not_found" ]];
     then
         echo "Content-type: text/html"
         echo ""
@@ -14,8 +15,11 @@ if [[ "$(echo "$request" | jq -r .error)" == "not_found" ]];
     else
 
         echo "Content-type: text/html"
+        # Долгоживущая кука, чтобы человек не мог отметиться за других, кодируем base64
+        echo "Set-Cookie: llc=aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1GVHVfbmRuaC13YyZhYl9jaGFubmVsPSVEMCU5MCVEMCVCQiVEMCVCNSVEMCVCQSVEMSU4MSVEMCVCNSVEMCVCOSVEMCU5RiVEMCVCNSVEMSU4MiVEMSU4MCVEMCVCRSVEMCVCMg==); expires=$(date -d '+12 hour' '+%a, %d %b %Y %H:%M:%S %Z'))"
         echo ""
         cat /var/www/html/access.html
         env
+
 
 fi
