cfde8097f868205de12be2549003765f14a561ca
kiryalemeshev <124894387+kiryalemeshev@users.noreply.github.com>
2023-06-15 16:49:42 +0300

    Create access.sh

---
+++ data/access.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env ash
+
+
+cookie_allow="$(echo $HTTP_COOKIE | awk -F "=" '{ print $2 }')"
+llc="$(echo $HTTP_COOKIE | awk -F "=" '{ print $1 }')"
+
+qr_allow_request="$(http --ignore-stdin "http://admin:admin@couchserver:5950/qrallow/$cookie_allow")"
+
+# Проверяем, есть ли у нас qrallow пользователя
+if [[ "$(echo "$qr_allow_request" | jq -r .error)" == "not_found" ]];
+    then
+        echo "Content-type: text/html"
+        echo ""
+        echo '<head>   <meta http-equiv="refresh" content="0; URL=/cgi/index.sh" /> </head>'
+    else
+
+        echo "Content-type: text/html"
+        # Долгоживущая кука, чтобы человек не мог отметиться за других, кодируем base64
+        echo "Set-Cookie: llc=aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1GVHVfbmRuaC13YyZhYl9jaGFubmVsPSVEMCU5MCVEMCVCQiVEMCVCNSVEMCVCQSVEMSU4MSVEMCVCNSVEMCVCOSVEMCU5RiVEMCVCNSVEMSU4MiVEMSU4MCVEMCVCRSVEMCVCMg==); expires=$(date -d '+12 hour' '+%a, %d %b %Y %H:%M:%S %Z'))"
+        echo ""
+        cat /var/www/html/access.html
+        env
+
+
+fi
